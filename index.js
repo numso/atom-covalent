51ece6118ffa749c44bfd899f18f0d0da0459790
Dallin Osmun <dosmun@gmail.com>
2015-01-24 13:29:55 -0700

    6to5 loader loads firepad wrong

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
+// 6to5's require doesn't load this right...
+var Firepad = require('./lib/firepad');
+
 require('6to5/register');
-module.exports = require('./lib/covalent');
+module.exports = require('./lib/covalent')(Firepad);
