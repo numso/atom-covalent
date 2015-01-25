a85aa86911f3525c36ee4a0d4d7f219a1021f711
Dallin Osmun <dosmun@gmail.com>
2015-01-25 10:45:13 -0700

    all is well again

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-// 6to5's require doesn't load this right...
-var firepad = require('./lib/firepad-patches/firepad');
-
 require('6to5/register')({
 
   ignore: /node_modules/,
 
-  extensions: ['.jsx', '.es6.js']
+  extensions: ['.es6.js']
 
 });
 
-// I'm putting all my changes in here and trying to leave Firepad itself unchanged
-firepad.AtomAdapter = require('./lib/firepad-patches/atom-adapter')(firepad);
-
-module.exports = require('./lib/covalent')(firepad.Firepad);
+module.exports = require('./lib/covalent');
